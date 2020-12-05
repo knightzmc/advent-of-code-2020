d9095613eac7eaeebbca53094a66311a5af59b32
Alexander Wood <alexljwood24@hotmail.co.uk>
2020-12-05 20:41:30 +0000

    A bit of cleanup

--- part4.clj
+++ part4.clj
@@ -8,6 +8,10 @@
 
 (def required-parts #{"byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"})
 
+(defn to-int [s] (try
+                   (Integer/parseInt s)
+                   (catch NumberFormatException _ nil)))
+
 (defn valid [parts] (every? #(contains? parts %) required-parts))
 
 (def passports (map #(str/replace % "\n" " ") (str/split (slurp "day4.txt") #"\n\n")))
@@ -19,24 +23,20 @@
 
 (defn valid-height? [height]
   (if (str/ends-with? height "cm")
-    (<= 150 (Integer/parseInt (subs height 0 (- (count height) 2))) 193)
+    (<= 150 (to-int (subs height 0 (- (count height) 2))) 193)
     (if (str/ends-with? height "in")
-        (<= 59 (Integer/parseInt (subs height 0 (- (count height) 2))) 76)
+        (<= 59 (to-int (subs height 0 (- (count height) 2))) 76)
         false)))
-
-(defn is-int? [s] (try
-                    (do (Integer/parseInt s) true)
-                    (catch NumberFormatException _ false)))
 
 (defn part-valid? [[key value]]
   (case key
-    "byr" (<= 1920 (Integer/parseInt value) 2002)
-    "iyr" (<= 2010 (Integer/parseInt value) 2020)
-    "eyr" (<= 2020 (Integer/parseInt value) 2030)
+    "byr" (<= 1920 (to-int value) 2002)
+    "iyr" (<= 2010 (to-int value) 2020)
+    "eyr" (<= 2020 (to-int value) 2030)
     "hgt" (valid-height? value)
     "hcl" (re-matches #"#[0-9a-f]{6}" value)
     "ecl" (contains? #{"amb" "blu" "brn" "gry" "grn" "hzl" "oth"} value)
-    "pid" (and (== (count value) 9) (is-int? (str/replace value #"^0+(?!$)" "")))
+    "pid" (and (== (count value) 9) (to-int (str/replace value #"^0+(?!$)" "")))
     "cid" true))
 
 (defn parts-valid? [parts]
