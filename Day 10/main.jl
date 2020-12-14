358806228c043775d4d190c9095cef0285354f15
Alexander Wood <alexljwood24@hotmail.co.uk>
2020-12-14 16:45:12 +0000

    Part 2 of day 10 finally works :D

--- Day 10/main.jl
+++ Day 10/main.jl
@@ -2,20 +2,20 @@ file= open("./input.txt")
 lines = readlines(file)
 close(file)
 
-nums = map(num -> parse(Int, num), lines)
+nums = map(num -> parse(UInt8, num), lines)
 
 struct Adapter
-    data::Int64
+    data::UInt8
     child::Union{Adapter, Nothing}
 end 
 
-function get_min_adapter_tree(inputs::Array{Int64}, next = 0)
+function get_min_adapter_tree(inputs::Array{UInt8}, next = 0)
     if isempty(inputs)
         return nothing
     end
 
     if next == 0
-        append!(inputs, [maximum(inputs) + 3])
+        append!(inputs, maximum(inputs) + 3)
     end
     
     valid = filter(num -> num - next <= 3, inputs)
@@ -23,6 +23,8 @@ function get_min_adapter_tree(inputs::Array{Int64}, next = 0)
 
     return Adapter(nextvalid, get_min_adapter_tree(filter(i -> i != nextvalid, inputs), nextvalid))
 end
+
+
 
 function get_part1_output(elem)
     sums = [0, 0, 0]
@@ -37,9 +39,58 @@ function get_part1_output(elem)
     return (sums[1], sums[2], sums[3])
 end
 
-tree = get_min_adapter_tre(nums)
+tree = get_min_adapter_tree(nums)
 
 a, _, c = get_part1_output(tree)
-println(a)
-println(c)
+
 println(a * c)
+
+#Part 2
+
+struct Tree
+    data::UInt8
+    children::Array{Tree}
+end
+
+
+children_memoiser = Dict{Int, Tree}()
+function get_all_adapter_tree(inputs::Array{UInt8}, next = 0)
+
+    valid = filter(num -> (num - next <= 3) && (num - next > 0), inputs)
+
+    children = map(num -> begin
+            get!(children_memoiser, num) do
+                newinputs = filter(i -> i != num, inputs)
+                get_all_adapter_tree(newinputs, num)
+            end
+    end, valid)
+
+
+    return Tree(next, children)
+end
+
+tree = get_all_adapter_tree(nums)
+
+combo_memoiser = Dict{Tree, Int}()
+function get_combinations(tree, max)
+    if isempty(tree.children)
+        if tree.data == max
+            return 1
+        else
+             return 0
+        end
+    end
+    
+    matching = 0
+
+    for child in tree.children
+        child_combos = get!(combo_memoiser, child) do
+            get_combinations(child, max)
+        end
+        matching += child_combos
+    end
+    
+    return matching
+end
+
+println(get_combinations(tree, maximum(nums)))
